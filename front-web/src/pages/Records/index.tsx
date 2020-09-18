5de3a21fd846bce96a0fdb1e23ee4f6b9c926c2d
unknown <wesleymna98@gmail.com>
2020-09-18 10:43:07 -0300

    Frontend added

---
+++ front-web/src/pages/Records/index.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './styles.css';
+import { RecordResponse } from './types';
+import { formatDate } from './helpers';
+import Pagination from './Pagination';
+import Filters from '../../components/Filters';
+
+const BASE_URL = 'https://ds-pesquisa-wesley.herokuapp.com'
+
+const Records = () => {
+    const [ recordResponse, setRecordResponse ] = useState<RecordResponse>();
+    const [ activePage, setActivePage ] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/records?linesPerPage=12&page=${activePage}`)
+        .then(response => setRecordResponse(response.data));
+    }, [activePage]);
+
+    const handlePageChange = (index: number) => {
+        setActivePage(index)
+    }
+
+    return (
+        <div className="page-container">
+            <Filters link="/charts" linkText="VER GRÁFICO"/>
+            <table className="records-table" cellPadding="0" cellSpacing="0">
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NOME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TÍTULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {recordResponse?.content.map(record => (
+                        <tr key={record.id}>
+                            <td>{formatDate(record.moment)}</td>
+                            <td>{record.name}</td>
+                            <td>{record.age}</td>
+                            <td className="text-secondary">{record.gamePlatform}</td>
+                            <td>{record.genreName}</td>
+                            <td className="text-primary">{record.gameTitle}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+            <Pagination 
+                activePage={activePage} 
+                goToPage={handlePageChange}
+                totalPages={recordResponse?.totalPages}
+            />
+        </div>
+    );
+}
+
+export default Records;
